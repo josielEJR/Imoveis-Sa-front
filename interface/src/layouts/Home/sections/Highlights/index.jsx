d595d4650348a7d0c21c73f49bf73157660bb45a
akalinoxd <jvitorcavalcante18@gmail.com>
2024-07-17 12:36:02 -0300

    Arrumando bug na seção de highlights

--- interface/src/layouts/Home/sections/Highlights/index.jsx
+++ interface/src/layouts/Home/sections/Highlights/index.jsx
@@ -9,159 +9,156 @@ const Hightlights = () => {
     const [selected, setSelected] = useState(0)
     const [prev, setPrev] = useState(selected)
     const [key, setKey] = useState(0)
+    const [pageLayout, setPageLayout] = useState(window.innerWidth)
     const [products, setProducts] = useState([
-        [
-            {
-                imagem: "https://grupospimovel.s3.amazonaws.com/Imagens/Noticias/Sistema/mercado-imobiliario/0201_valor_venal_b.jpg",
-                bairro: "JARDIM EUROPA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "ALUGUEL",
-                preco: 4000,
-                area: 100,
-                quartos: 4,
-                banheiros: 3,
-                vagas: 2,
-                redirect: "/imoveis"
-            },
-            {
-                imagem: "https://queroficarrico.com/blog/wp-content/uploads/2017/06/como_investir_em_imoveis.jpg",
-                bairro: "ITAIM PAULISTA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "VENDA",
-                preco: 130000,
-                area: 50,
-                quartos: 2,
-                banheiros: 1,
-                vagas: 1,
-                redirect: "/imoveis"
-            },
-            {
-                imagem: "https://resizedimgs.zapimoveis.com.br/crop/614x297/named.images.sp/46dd05db8786b3b0406e0ccf629c8018/sphera-panamby-no-vila-andrade-s-o-paulo.webp",
-                bairro: "PENHA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "ALUGUEL",
-                preco: 8000,
-                area: 200,
-                quartos: 8,
-                banheiros: 6,
-                vagas: 4,
-                redirect: "/imoveis"
-            }
-        ],
-        [
-            {
-                imagem: "https://queroficarrico.com/blog/wp-content/uploads/2017/06/como_investir_em_imoveis.jpg",
-                bairro: "ITAIM PAULISTA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "VENDA",
-                preco: 130000,
-                area: 50,
-                quartos: 2,
-                banheiros: 1,
-                vagas: 1,
-                redirect: "/imoveis"
-            },
-            {
-                imagem: "https://resizedimgs.zapimoveis.com.br/crop/614x297/named.images.sp/46dd05db8786b3b0406e0ccf629c8018/sphera-panamby-no-vila-andrade-s-o-paulo.webp",
-                bairro: "PENHA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "ALUGUEL",
-                preco: 8000,
-                area: 200,
-                quartos: 8,
-                banheiros: 6,
-                vagas: 4,
-                redirect: "/imoveis"
-            },
-            {
-                imagem: "https://grupospimovel.s3.amazonaws.com/Imagens/Noticias/Sistema/mercado-imobiliario/0201_valor_venal_b.jpg",
-                bairro: "JARDIM EUROPA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "ALUGUEL",
-                preco: 4000,
-                area: 100,
-                quartos: 4,
-                banheiros: 3,
-                vagas: 2,
-                redirect: "/imoveis"
-            }
-        ],
-        [
-            {
-                imagem: "https://resizedimgs.zapimoveis.com.br/crop/614x297/named.images.sp/46dd05db8786b3b0406e0ccf629c8018/sphera-panamby-no-vila-andrade-s-o-paulo.webp",
-                bairro: "PENHA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "ALUGUEL",
-                preco: 8000,
-                area: 200,
-                quartos: 8,
-                banheiros: 6,
-                vagas: 4,
-                redirect: "/imoveis"
-            },
-            {
-                imagem: "https://grupospimovel.s3.amazonaws.com/Imagens/Noticias/Sistema/mercado-imobiliario/0201_valor_venal_b.jpg",
-                bairro: "JARDIM EUROPA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "ALUGUEL",
-                preco: 4000,
-                area: 100,
-                quartos: 4,
-                banheiros: 3,
-                vagas: 2,
-                redirect: "/imoveis"
-            },
-            {
-                imagem: "https://queroficarrico.com/blog/wp-content/uploads/2017/06/como_investir_em_imoveis.jpg",
-                bairro: "ITAIM PAULISTA",
-                cidade: "SÃO PAULO - SP",
-                tipo: "VENDA",
-                preco: 130000,
-                area: 50,
-                quartos: 2,
-                banheiros: 1,
-                vagas: 1,
-                redirect: "/imoveis"
-            }
-        ]
+        // [
+        //     {
+        //         imagem: "https://grupospimovel.s3.amazonaws.com/Imagens/Noticias/Sistema/mercado-imobiliario/0201_valor_venal_b.jpg",
+        //         bairro: "JARDIM EUROPA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "ALUGUEL",
+        //         preco: 4000,
+        //         area: 100,
+        //         quartos: 4,
+        //         banheiros: 3,
+        //         vagas: 2,
+        //         redirect: "/imoveis"
+        //     },
+        //     {
+        //         imagem: "https://queroficarrico.com/blog/wp-content/uploads/2017/06/como_investir_em_imoveis.jpg",
+        //         bairro: "ITAIM PAULISTA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "VENDA",
+        //         preco: 130000,
+        //         area: 50,
+        //         quartos: 2,
+        //         banheiros: 1,
+        //         vagas: 1,
+        //         redirect: "/imoveis"
+        //     },
+        //     {
+        //         imagem: "https://resizedimgs.zapimoveis.com.br/crop/614x297/named.images.sp/46dd05db8786b3b0406e0ccf629c8018/sphera-panamby-no-vila-andrade-s-o-paulo.webp",
+        //         bairro: "PENHA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "ALUGUEL",
+        //         preco: 8000,
+        //         area: 200,
+        //         quartos: 8,
+        //         banheiros: 6,
+        //         vagas: 4,
+        //         redirect: "/imoveis"
+        //     }
+        // ],
+        // [
+        //     {
+        //         imagem: "https://queroficarrico.com/blog/wp-content/uploads/2017/06/como_investir_em_imoveis.jpg",
+        //         bairro: "ITAIM PAULISTA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "VENDA",
+        //         preco: 130000,
+        //         area: 50,
+        //         quartos: 2,
+        //         banheiros: 1,
+        //         vagas: 1,
+        //         redirect: "/imoveis"
+        //     },
+        //     {
+        //         imagem: "https://resizedimgs.zapimoveis.com.br/crop/614x297/named.images.sp/46dd05db8786b3b0406e0ccf629c8018/sphera-panamby-no-vila-andrade-s-o-paulo.webp",
+        //         bairro: "PENHA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "ALUGUEL",
+        //         preco: 8000,
+        //         area: 200,
+        //         quartos: 8,
+        //         banheiros: 6,
+        //         vagas: 4,
+        //         redirect: "/imoveis"
+        //     },
+        //     {
+        //         imagem: "https://grupospimovel.s3.amazonaws.com/Imagens/Noticias/Sistema/mercado-imobiliario/0201_valor_venal_b.jpg",
+        //         bairro: "JARDIM EUROPA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "ALUGUEL",
+        //         preco: 4000,
+        //         area: 100,
+        //         quartos: 4,
+        //         banheiros: 3,
+        //         vagas: 2,
+        //         redirect: "/imoveis"
+        //     }
+        // ],
+        // [
+        //     {
+        //         imagem: "https://resizedimgs.zapimoveis.com.br/crop/614x297/named.images.sp/46dd05db8786b3b0406e0ccf629c8018/sphera-panamby-no-vila-andrade-s-o-paulo.webp",
+        //         bairro: "PENHA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "ALUGUEL",
+        //         preco: 8000,
+        //         area: 200,
+        //         quartos: 8,
+        //         banheiros: 6,
+        //         vagas: 4,
+        //         redirect: "/imoveis"
+        //     },
+        //     {
+        //         imagem: "https://grupospimovel.s3.amazonaws.com/Imagens/Noticias/Sistema/mercado-imobiliario/0201_valor_venal_b.jpg",
+        //         bairro: "JARDIM EUROPA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "ALUGUEL",
+        //         preco: 4000,
+        //         area: 100,
+        //         quartos: 4,
+        //         banheiros: 3,
+        //         vagas: 2,
+        //         redirect: "/imoveis"
+        //     },
+        //     {
+        //         imagem: "https://queroficarrico.com/blog/wp-content/uploads/2017/06/como_investir_em_imoveis.jpg",
+        //         bairro: "ITAIM PAULISTA",
+        //         cidade: "SÃO PAULO - SP",
+        //         tipo: "VENDA",
+        //         preco: 130000,
+        //         area: 50,
+        //         quartos: 2,
+        //         banheiros: 1,
+        //         vagas: 1,
+        //         redirect: "/imoveis"
+        //     }
+        // ]
     ])
 
-    // useEffect(() => {
-    //     const myHeaders = new Headers();
-    //     myHeaders.append("Content-Type", "application/json");
+    useEffect(() => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
 
-    //     const requestOptions = {
-    //         method: "GET",
-    //         headers: myHeaders,
-    //         redirect: "follow"
-    //     };
+        const requestOptions = {
+            method: "GET",
+            headers: myHeaders,
+            redirect: "follow"
+        };
 
-    //     fetch("http://localhost:3001/imoveis/ordenarimovelqualidade", requestOptions)
-    //         .then((response) => response.text())
-    //         .then((result) => JSON.parse(result))
-    //         .then((result) => {
-    //             const arrayAux = [[], [], []]
-    //             result.forEach((elem, index) => {
-    //                 if (index <= 2) {
-    //                     arrayAux[0].push(elem)
-    //                 } else if (index <= 5) {
-    //                     arrayAux[1].push(elem)
-    //                 } else if (index <= 8) {
-    //                     arrayAux[2].push(elem)
-    //                 }
-    //             })
+        fetch("http://localhost:3001/imoveis/ordenarimovelqualidade", requestOptions)
+            .then((response) => response.text())
+            .then((result) => JSON.parse(result))
+            .then((result) => {
+                const arrayAux = [[], [], []]
+                result.forEach((elem, index) => {
+                    if (index <= 2) {
+                        arrayAux[0].push(elem)
+                    } else if (index <= 5) {
+                        arrayAux[1].push(elem)
+                    } else if (index <= 8) {
+                        arrayAux[2].push(elem)
+                    }
+                })
 
-    //             setProducts(arrayAux)
-    //         })
-    //         .catch((error) => console.error(error));
-    // }, [])
-
-    function handleResize() {
-        console.log(window.outerWidth)
-    }
+                setProducts(arrayAux)
+            })
+            .catch((error) => console.error(error));
+    }, [])
 
     const setLayout = () => {
-        if (window.outerWidth <= 950) {
+        if (pageLayout <= 950) {
             return (
                 <Container>
                     <Title>
@@ -297,6 +294,11 @@ const Hightlights = () => {
     }
 
     useEffect(() => {
+
+        function handleResize() {
+            setPageLayout(window.innerWidth)
+        }
+
         window.addEventListener('resize', handleResize)
 
         return _ => {
