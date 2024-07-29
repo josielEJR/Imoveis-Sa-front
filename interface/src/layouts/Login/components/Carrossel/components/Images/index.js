06096e1f6b302d7d3b1bd1e89a9b861e1abeafa8
giga <bgabrielsoares26@gmail.com>
2024-07-29 16:09:01 -0300

    Layout-Login-Finalizado

---
+++ interface/src/layouts/Login/components/Carrossel/components/Images/index.js
@@ -0,0 +1,16 @@
+const config = [
+{
+    title: 'image1',
+    Image: "https://images.adsttc.com/media/images/624c/abf4/0ba6/da01/66c7/d26d/newsletter/014-casa-dotta-galeria-733.jpg?1649191946",
+},
+{
+    title: 'image2',
+    Image: "https://imgs2.cdn-imobibrasil.com.br/imagens/imoveis/20231031193722165.jpg",
+},
+{   
+    title: 'image3',
+    Image: "https://www.imperiovimoveis.com.br/assets/img/banner-slider-2.jpg",
+},
+]
+
+export default config
---
+++ interface/src/layouts/Login/components/Login/style.js
@@ -0,0 +1,135 @@
+import styled, { keyframes } from "styled-components"
+
+export const Wrapper = styled.div`
+
+`
+
+export const Container = styled.div`
+    justify-content: center;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 800px;
+    height: 600px;
+    background: rgba(255, 255, 255, 0.1);
+    backdrop-filter: blur(10px);
+    border: 1px solid white;
+    transform: translate(-50%, -50%);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 100px;
+    border-radius: 10px;
+    
+      @media (max-width: 900px) {
+        width: 90%;
+        height: 650px;
+        padding: 50px;
+        
+    }
+    `
+
+export const InnerWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+
+      @media (max-width: 900px ) {
+        
+    }
+    `
+
+export const Title = styled.h2`
+    margin-bottom: 20px;
+    font-size: 30px;
+    color: white;
+    `
+
+export const Input = styled.input`
+    width: 100%;
+    height: 60px;
+    padding: 20px;
+    margin: 20px;
+    border: 1px solid ${({ error }) => (error ? 'red' : '#ccc')};
+    border-radius: 25px;
+    box-sizing: border-box;
+    font-size: 15px;
+
+    @media (max-width: 900px ) {
+   
+    }
+    `
+
+export const OptionsContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    width: 80%;
+    margin-top: 40px;
+
+    @media (max-width: 540px ) {
+      flex-direction: column;
+      gap: 20px;
+      align-items: center;
+  }
+    `
+
+export const RememberMe = styled.label`
+    display: flex;
+    align-items: center;
+    font-size: 17px;
+    color: white;
+    `
+
+export const Checkbox = styled.input`
+    margin-right: 8px;
+    `
+
+export const ForgotPassword = styled.a`
+    font-size: 17px;
+    color: white;
+    cursor: pointer;
+    
+&:hover {
+    text-decoration: underline;
+}
+`
+
+const clickAnimation = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(0.9);
+  }
+  100% {
+    transform: scale(1);
+  }
+`
+
+export const LoginButton = styled.button`
+    width: 100%;
+    padding: 10px;
+    margin-top: 40px;
+    border-radius: 20px;
+    background-color: black;
+    color: white;
+    font-size: 17px;
+    cursor: pointer;
+    animation: ${({ animate }) => animate ? clickAnimation: 'none'} 0.3s ease-in-out;
+
+    @media (max-width: 900px) {
+      width: 100%;
+    }
+    `
+
+export const Signup = styled.a`
+    font-size: 20px;
+    color: white;
+    cursor: pointer;
+    margin-top: 50px;
+  
+  &:hover {
+    text-decoration: underline;
+  }
+`
