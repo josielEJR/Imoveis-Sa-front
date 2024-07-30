03fbfdce3587d3e02253acc8578ebda3576efb15
akalinoxd <jvitorcavalcante18@gmail.com>
2024-07-30 11:40:46 -0300

    Finalizando footer

--- interface/src/Pages/Home/Home.js
+++ interface/src/Pages/Home/Home.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import Hero from '../../layouts/Home/sections/Hero/index.jsx'
 import Depoimentos from '../../layouts/Home/sections/Depoimentos/index.jsx'
-import {Wrapper} from './style'
 import Footer from '../../components/Footer/index.jsx'
+
+import { Wrapper } from './style'
 
 const Home = () => {
   return (
