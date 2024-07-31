ff357adae8ee1b3d3939469439603f0f7546aaa2
giga <bgabrielsoares26@gmail.com>
2024-07-31 15:23:29 -0300

    Tela de Cadastro e Olhinho da Senha

--- interface/src/layouts/Login/components/Carrossel/components/Images/index.js
+++ interface/src/layouts/Login/components/Carrossel/components/Images/index.js
@@ -1,15 +1,15 @@
 const config = [
 {
     title: 'image1',
-    Image: "https://images.adsttc.com/media/images/624c/abf4/0ba6/da01/66c7/d26d/newsletter/014-casa-dotta-galeria-733.jpg?1649191946",
+    Image: "https://img.freepik.com/fotos-gratis/renderizacao-3d-de-uma-casa-de-madeira_23-2151264475.jpg?t=st=1722441187~exp=1722444787~hmac=0fe38a5c0234f99bdbc123da92f0c360c069d5eb05ac0a635f9f68c6f3a21345&w=1800",
 },
 {
     title: 'image2',
-    Image: "https://imgs2.cdn-imobibrasil.com.br/imagens/imoveis/20231031193722165.jpg",
+    Image: "https://img.freepik.com/fotos-gratis/design-de-interiores-moderna-sala-de-estar_23-2150794647.jpg?t=st=1722441189~exp=1722444789~hmac=4d1cd520540bff391abf75fa13251608bb1af4c854704a7c4ad79a6a9a8d3073&w=1800",
 },
 {   
     title: 'image3',
-    Image: "https://www.imperiovimoveis.com.br/assets/img/banner-slider-2.jpg",
+    Image: "https://img.freepik.com/fotos-gratis/renderizacao-3d-de-uma-casa-de-madeira_23-2151264384.jpg?t=st=1722441219~exp=1722444819~hmac=c9d29fc0794621731b019c39a9ba42a38f5f60d85712ab821805c96a8865dcb8&w=1800",
 },
 ]
 
