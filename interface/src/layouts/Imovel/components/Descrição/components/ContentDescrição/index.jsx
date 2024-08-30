5cb5a1238f58bea2a4a21f5b90c3d9ecfbe46ac0
Josiel Ejr <juninhojoka11@gmail.com>
2024-08-30 16:29:06 -0300

    Componente DropHover isolado

--- interface/src/layouts/Imovel/components/Descrição/components/ContentDescrição/index.jsx
+++ interface/src/layouts/Imovel/components/Descrição/components/ContentDescrição/index.jsx
@@ -1,29 +1,17 @@
 import React, { useState, useEffect } from 'react'
-import { Container, ContainerContent, ContainerIcon, ContainerInfo, Content, Icon, ContainerDescrição, Value, Wrapper, DescriçãoIcon, DropOver, TextDrop, Aba, Title, Text } from './style'
+
+import { Container, ContainerContent, ContainerIcon, ContainerInfo, Content, Icon, ContainerDescrição, Value, Wrapper, DescriçãoIcon, Text } from './style'
 import { TbRulerMeasure } from "react-icons/tb"
 import { IoCarSportOutline } from "react-icons/io5"
 import { LiaCouchSolid } from "react-icons/lia"
 import { TbBed } from "react-icons/tb"
 import { PiBathtub } from "react-icons/pi"
 import { MdInfoOutline } from "react-icons/md"
+import DropInfo from '../DropInfo'
 
 const ContentDescrição = ({ imovelID }) => {
   const [prodInfo, setProdInfo] = useState({})
   const [activeIcon, setActiveIcon] = useState(null)
-  const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 960)
-
-  const [leftPercentage, setLeftPercentage] = useState({
-    condominio: 5,
-    iptu: 5,
-    seguro: 5,
-    taxa: 5,
-  })
-  const [topPercentage, setTopPercentage] = useState({
-    condominio: 5,
-    iptu: 5,
-    seguro: 5,
-    taxa: 5,
-  })
 
   useEffect(() => {
     const requestOptions = {
@@ -75,62 +63,6 @@ const ContentDescrição = ({ imovelID }) => {
   const handleMouseEnter = (icon) => setActiveIcon(icon)
   const handleMouseLeave = () => setActiveIcon(null)
 
-  useEffect(() => {
-    const handleResize = () => {
-      const screenWidth = window.innerWidth
-      setIsSmallScreen(screenWidth <= 590)
-
-      if (screenWidth <= 590) {
-        setLeftPercentage({
-          condominio: 19,
-          iptu: 5,
-          seguro: 25,
-          taxa: 24,
-        })
-        setTopPercentage({
-          condominio: 50,
-          iptu: 59,
-          seguro: 68,
-          taxa: 76,
-        })
-      } else if (screenWidth <= 720) {
-        setLeftPercentage({
-          condominio: 40,
-          iptu: 27,
-          seguro: 45,
-          taxa: 45,
-        })
-      } else if (screenWidth <= 860) {
-        setLeftPercentage({
-          condominio: 30,
-          iptu: 30,
-          seguro: 35,
-          taxa: 35,
-        })
-      } else if (screenWidth <= 960) {
-        setLeftPercentage({
-          condominio: 25,
-          iptu: 17,
-          seguro: 30,
-          taxa: 30,
-        })
-      } else {
-        setLeftPercentage({
-          condominio: 5,
-          iptu: 5,
-          seguro: 5,
-          taxa: 5,
-        })
-      }
-    }
-
-    handleResize()
-
-    window.addEventListener('resize', handleResize)
-
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
-
   return (
     <Wrapper>
       <Container>
@@ -149,18 +81,7 @@ const ContentDescrição = ({ imovelID }) => {
                 onMouseLeave={handleMouseLeave}
               />
               {activeIcon === 'condominio' && (
-                <DropOver
-                  leftPercentage={`${leftPercentage.condominio}%`}
-                  topPercentage={`${topPercentage.condominio}%`}
-                >
-                  <Title>
-                    Condominio
-                  </Title>
-                  <Aba />
-                  <TextDrop>
-                    Valor sujeito a alteração conforme deliberações condominiais, podendo, ainda, variar de acordo com as despesas fixas e eventuais do condomínio (ex.: água, luz, conservação e manutenção do prédio, entre outros).
-                  </TextDrop>
-                </DropOver>
+                <DropInfo activeIcon={activeIcon} iconName="condominio" />
               )}
             </Content>
             <Content>
@@ -173,18 +94,7 @@ const ContentDescrição = ({ imovelID }) => {
                 onMouseLeave={handleMouseLeave}
               />
               {activeIcon === 'iptu' && (
-                <DropOver
-                  leftPercentage={`${leftPercentage.iptu}%`}
-                  topPercentage={`${topPercentage.iptu}%`}
-                >
-                  <Title>
-                    IPTU
-                  </Title>
-                  <Aba />
-                  <TextDrop>
-                    Valor sujeito a alteração conforme deliberações condominiais, podendo, ainda, variar de acordo com as despesas fixas e eventuais do condomínio (ex.: água, luz, conservação e manutenção do prédio, entre outros).
-                  </TextDrop>
-                </DropOver>
+                <DropInfo activeIcon={activeIcon} iconName="iptu" />
               )}
             </Content>
             <Content>
@@ -197,18 +107,7 @@ const ContentDescrição = ({ imovelID }) => {
                 onMouseLeave={handleMouseLeave}
               />
               {activeIcon === 'seguro' && (
-                <DropOver
-                  leftPercentage={`${leftPercentage.seguro}%`}
-                  topPercentage={`${topPercentage.seguro}%`}
-                >
-                  <Title>
-                    Seguro incêndio
-                  </Title>
-                  <Aba />
-                  <TextDrop>
-                    Valor sujeito a alteração conforme deliberações condominiais, podendo, ainda, variar de acordo com as despesas fixas e eventuais do condomínio (ex.: água, luz, conservação e manutenção do prédio, entre outros).
-                  </TextDrop>
-                </DropOver>
+                <DropInfo activeIcon={activeIcon} iconName="seguro" />
               )}
             </Content>
             <Content>
@@ -221,18 +120,7 @@ const ContentDescrição = ({ imovelID }) => {
                 onMouseLeave={handleMouseLeave}
               />
               {activeIcon === 'taxa' && (
-                <DropOver
-                  leftPercentage={`${leftPercentage.taxa}%`}
-                  topPercentage={`${topPercentage.taxa}%`}
-                >
-                  <Title>
-                    Taxa de serviço
-                  </Title>
-                  <Aba />
-                  <TextDrop>
-                    Valor sujeito a alteração conforme deliberações condominiais, podendo, ainda, variar de acordo com as despesas fixas e eventuais do condomínio (ex.: água, luz, conservação e manutenção do prédio, entre outros).
-                  </TextDrop>
-                </DropOver>
+                <DropInfo activeIcon={activeIcon} iconName="taxa" />
               )}
             </Content>
           </ContainerDescrição>
@@ -295,7 +183,6 @@ const ContentDescrição = ({ imovelID }) => {
           </ContainerInfo>
         </ContainerIcon>
       </Container>
-
     </Wrapper>
   )
 }
---
+++ interface/src/layouts/Imovel/components/Descrição/components/DropInfo/index.jsx
@@ -0,0 +1,102 @@
+import React, { useEffect, useState } from 'react'
+import { Aba, DropHover, TextDrop, Title, Wrapper } from './style'
+
+const DropInfo = ({ activeIcon, iconName }) => {
+    const [setIsSmallScreen] = useState(window.innerWidth <= 960)
+
+    const [leftPercentage, setLeftPercentage] = useState({
+        condominio: 5,
+        iptu: 5,
+        seguro: 5,
+        taxa: 5,
+    })
+    const [topPercentage, setTopPercentage] = useState({
+        condominio: 0,
+        iptu: 0,
+        seguro: 0,
+        taxa: 0,
+    })
+
+    useEffect(() => {
+        const handleResize = () => {
+            const screenWidth = window.innerWidth
+            setIsSmallScreen(screenWidth <= 590)
+
+            if (screenWidth <= 590) {
+                setLeftPercentage({
+                    condominio: 19,
+                    iptu: 5,
+                    seguro: 25,
+                    taxa: 24,
+                })
+                setTopPercentage({
+                    condominio: 50,
+                    iptu: 59,
+                    seguro: 68,
+                    taxa: 76,
+                })
+            } else if (screenWidth <= 722) {
+                setLeftPercentage({
+                    condominio: 36,
+                    iptu: 24,
+                    seguro: 44,
+                    taxa: 43,
+                })
+                setTopPercentage({
+                    condominio: 36.5,
+                    iptu: 45.5,
+                    seguro: 54,
+                    taxa: 63,
+                })
+            } else if (screenWidth <= 960) {
+                setLeftPercentage({
+                    condominio: 30,
+                    iptu: 27,
+                    seguro: 45,
+                    taxa: 30,
+                })
+                setTopPercentage({
+                    condominio: 36.5,
+                    iptu: 45,
+                    seguro: 54,
+                    taxa: 63,
+                })
+            } else {
+                setLeftPercentage({
+                    condominio: 5,
+                    iptu: 5,
+                    seguro: 5,
+                    taxa: 5,
+                })
+            }
+        }
+
+        handleResize()
+
+        window.addEventListener('resize', handleResize)
+
+        return () => window.removeEventListener('resize', handleResize)
+    }, [])
+
+    if (!iconName || activeIcon !== iconName) {
+        return null
+    }
+
+    return (
+        <Wrapper>
+            <DropHover
+                leftPercentage={`${leftPercentage[iconName]}%`} topPercentage={`${topPercentage[iconName]}%`}
+            >
+                <Title>
+                    {iconName.charAt(0).toUpperCase() + iconName.slice(1)}
+                </Title>
+                <Aba />
+                <TextDrop>
+                    Valor sujeito a alteração conforme deliberações condominiais, podendo, ainda, variar de acordo com as despesas fixas e eventuais do condomínio (ex.: água, luz, conservação e manutenção do prédio, entre outros).
+                </TextDrop>
+            </DropHover>
+        </Wrapper>
+    )
+}
+
+export default DropInfo
