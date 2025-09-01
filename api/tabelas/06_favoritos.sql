a1bcc18edb854720074d048e0f02fbd798abc495
Josiel Evangelista Junior <125618119+josielEJR@users.noreply.github.com>
2025-09-01 11:37:36 -0300

    Delete api directory

--- api/tabelas/06_favoritos.sql
+++
@@ -1,9 +0,0 @@
-USE websiteInfo;
-
-CREATE TABLE IF NOT EXISTS favoritos (
-    clienteID INT,
-    imovelID INT,
-    PRIMARY KEY (clienteID, imovelID),
-    FOREIGN KEY (clienteID) REFERENCES clientes(clienteId) ON DELETE CASCADE,
-    FOREIGN KEY (imovelID) REFERENCES imoveis(imoveisID) ON DELETE CASCADE
-);
